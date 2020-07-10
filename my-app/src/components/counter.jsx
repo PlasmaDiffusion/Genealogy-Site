95ab146fc470b6bcda84089140f383a6849dba79
PlasmaDiffusion <Scott50000@gmail.com>
2020-07-10 14:23:17 -0400

    Counter component class

---
+++ my-app/src/components/counter.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+    imageUrl: "https://picsum.photos/200",
+    tags: ["tag1", "tag2", "tag3"],
+  };
+
+  //Alternate is <span style={{ fontSize: 30 }}
+  styles = {
+    fontSize: 10,
+    fontWeight: "bold",
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <span className={this.GetBadgeClasses()}>{this.FormatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      </React.Fragment>
+    );
+  }
+
+  GetBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  FormatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
