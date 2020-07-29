b6541cca97a95b7435fcfadd1da62fbd8b3e3867
PlasmaDiffusion <Scott50000@gmail.com>
2020-07-29 14:41:10 -0400

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -56,7 +56,7 @@ export default withOktaAuth(
       return (
         <Router>
           <div>
-            <Link to="">Home</Link>
+            <Link to=""> Home </Link>
             <br></br>
             <hr />
 
