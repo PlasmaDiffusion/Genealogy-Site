0042974f605fbca472c4db91459239707b001d74
PlasmaDiffusion <Scott50000@gmail.com>
2021-03-01 19:27:18 -0500

    Bigger timeout for tests

--- test/test.js
+++ test/test.js
@@ -5,18 +5,9 @@ const mongoose = require("mongoose");
 
 var tests = 0;
 
-// Will run after every test in every file
-afterEach(function () {
-  tests++;
-  if (tests >= 4) {
-    mongoose.disconnect();
-    console.log("Sequlize connection closed");
-  }
-});
-
-/*after(async function () {
+after(async function () {
   mongoose.disconnect();
   console.log("Connection closed");
-});*/
+});
 
 //testRoutes.js will do the rest
