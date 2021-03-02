029e19d30ec0a78a6c060fb730052f4f5417f741
PlasmaDiffusion <Scott50000@gmail.com>
2021-03-01 19:56:42 -0500

    Update test.js

--- test/test.js
+++ test/test.js
@@ -3,21 +3,9 @@ var assert = require("assert");
 
 const mongoose = require("mongoose");
 
-//Data to connect to the database
-const uri =
-  "mongodb+srv://admin:" +
-  process.env.MONGO_PASS +
-  "@cluster0-qjfez.mongodb.net/geneology?retryWrites=true&w=majority";
-
-beforeEach(async () => {
-  await mongoose.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-});
-
-afterEach(async () => {
-  await mongoose.connection.close();
+after(function () {
+  mongoose.disconnect();
+  console.log("Connection closed");
 });
 
 //testRoutes.js will do the rest
