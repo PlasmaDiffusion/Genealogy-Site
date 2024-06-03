88dfda957cb298a0f71919b0b3effd15a02be1b4
Scott Cooper <Scott50000@gmail.com>
2024-06-03 17:12:32 -0400

    Use environment vars for server url (getUrl.js)

--- client/src/services/getUrl.js
+++ client/src/services/getUrl.js
@@ -2,12 +2,12 @@
 export function getClientUrl() {
   return process.env.NODE_ENV == "development"
     ? "http://localhost:3000"
-    : "https://genealogy-site.herokuapp.com";
+    : process.env.REACT_APP_SERVER_URL;
 }
 
 //3 Different possible server urls: development, testing or deployed
 export function getServerUrl() {
   if (process.env.NODE_ENV == "development") return "http://localhost:4000";
   else if (process.env.NODE_ENV == "test") return "";
-  else return "https://genealogy-site.herokuapp.com";
+  else return process.env.REACT_APP_SERVER_URL;
 }
